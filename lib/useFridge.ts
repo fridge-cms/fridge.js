b424005ac8abb299da92d04be7cdd71fc2f32f45
Mike Kruk <mike@ripeworks.com>
2021-03-31 12:21:52 -0400

    bail from useFridge hook if path does not exist

--- lib/useFridge.ts
+++ lib/useFridge.ts
@@ -1,7 +1,19 @@
 import useFetch from "use-http";
 
-const useFridge = (path: string, options: {} | any[], deps: any[] = []) => {
+const useFridge = (
+  path: string | null,
+  options: {} | any[],
+  deps: any[] = []
+) => {
   const hookDeps = Array.isArray(options) ? options : deps;
+
+  if (!path) {
+    return {
+      data: undefined,
+      loading: false,
+      error: false,
+    };
+  }
 
   return useFetch(
     `https://api.fridgecms.com/v2/${(path || "").replace(/^\//, "")}`,
