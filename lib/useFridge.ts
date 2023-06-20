a201c0cdfbc82a702c0b1a6a90fe1f2d6b3953de
Mike Kruk <mike@ripeworks.com>
2023-06-20 15:53:57 -0400

    make useFridge options optional

--- lib/useFridge.ts
+++ lib/useFridge.ts
@@ -1,8 +1,8 @@
 import useSWR from "swr";
 import fridge from "./fridge";
 
-const useFridge = (path: string | null, options: {}) => {
-  return useSWR(path, fridge, options || {});
+const useFridge = (path: string | null, options: {} = {}) => {
+  return useSWR(path, fridge, options);
 };
 
 export default useFridge;
